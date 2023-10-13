a17e93bcf87a150a62ecc166285ad3dfa0dc2989
woojinv <oh.woojin95@gmail.com>
2023-10-12 21:51:34 -0500

    display report of word counts

--- main.py
+++ main.py
@@ -1,36 +1,25 @@
-book = "frankenstein"
-
-with open(f"books/{book}.txt") as f:
-
-    print(f"Generating report for \"{book}\"")
-
-    file_contents = f.read()
+with open("books/frankenstein.txt", "r") as file:
+    file_contents = file.read()
+    
+    # print all the words
     words = file_contents.split()
 
-    print(f"Word count: {len(words)}")
+    word_count_dictionary = {}
+    for word in words:
+        lower = word.lower()
+        key = "".join([char for char in lower if lower.isalpha()])
 
-    letter_count_dict = {}
-    for char in file_contents:
-        key = char.lower()
-        if key in letter_count_dict:
-            letter_count_dict[key] += 1
+        if key in word_count_dictionary:
+            word_count_dictionary[key] += 1
         else:
-            letter_count_dict[key] = 1
+            word_count_dictionary[key] = 1
     
-    tuples_list = []
-    for key in letter_count_dict.keys():
-        if key.isalpha():
-            tuples_list.append((key, letter_count_dict[key]))
+    list_of_tuples = []
+    for key in word_count_dictionary:
+        list_of_tuples.append((key, word_count_dictionary[key]))
     
-    def myFunc(tuple):
-        return tuple[1]
-
-    tuples_list.sort(key=myFunc, reverse=True)
-
-    print("Letter count: ")
-    for tuple in tuples_list:
-        print(f"{tuple[0]}: {tuple[1]}")
-
-
-    
-
+    sorted_tuples = sorted(list_of_tuples, key=lambda item: item[1], reverse=True)
+    for item in sorted_tuples:
+        if item[1] > 5:
+            print(f"{item[0]}: {item[1]}")
+            
